ccb9e8b50644d026a31b6d0e6fc147310e29addf
Deepayan Sarkar <dsarkar@fhcrc.org>
2007-04-25 23:12:11 +0000

    Added a file parallel.R since latticeMethods.R is getting too congested (may split out into more files later).  Also added a parallel("formula", "flowSet") method.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowViz@24287 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/parallel.R
@@ -0,0 +1,137 @@
+
+## things are getting too crowded in latticeMethods.R, so start
+## splitting things out into different files.  This one is for
+## parallel coordinate plots.
+
+
+
+setMethod("parallel",
+          signature(x = "flowFrame", data = "missing"),
+          function(x, data, 
+                   reorder.by = function(x) var(x, na.rm = TRUE),
+                   time = "Time", exclude.time = TRUE,
+                   ...)
+      {
+          expr <- exprs(x)
+          column.names <- colnames(expr)
+          if (exclude.time) column.names <- column.names[column.names != time]
+          if (!is.null(reorder.by))
+          {
+              column.order <- rev(order(apply(expr[, column.names], 2, reorder.by)))
+              column.names <- column.names[column.order]
+          }
+          parallel(expr[, column.names],
+                   ...)
+      })
+
+
+
+setMethod("parallel",
+          signature(x = "formula", data = "flowSet"),
+          function(x, data, 
+                   reorder.by = function(x) var(x, na.rm = TRUE),
+                   time = "Time", exclude.time = TRUE,
+                   ...)
+      {
+          expr <- exprs(x)
+          column.names <- colnames(expr)
+          if (exclude.time) column.names <- column.names[column.names != time]
+          if (!is.null(reorder.by))
+          {
+              column.order <- rev(order(apply(expr[, column.names], 2, reorder.by)))
+              column.names <- column.names[column.order]
+          }
+          parallel(expr[, column.names], ...)
+      })
+
+
+## Formula is like  ~ . | a + b. The '.' part is currently ignored.
+
+setMethod("parallel",
+          signature(x = "formula", data = "flowSet"),
+          function(x, data,
+                   time = "Time", exclude.time = TRUE,
+                   filter = NULL,
+                   filterResults = NULL,
+                   xlab = NULL, ylab = NULL,
+                   ...)
+      {
+
+          if (!is.null(filter) && is.null(filterResults))
+              filterResults <- filter(data, filter)
+
+          pd <- pData(phenoData(data))
+          uniq.name <- createUniqueColumnName(pd)
+          ## ugly hack to suppress warnings about coercion introducing NAs
+          pd[[uniq.name]] <- factor(sampleNames(data))
+          channels <- x[[2]]
+          if (length(channels) == 3)
+          {
+              channels <- channels[[2]]
+              x[[2]][[2]] <- as.name(uniq.name)
+          }
+          else x[[2]] <- as.name(uniq.name)
+
+          ## channels is going to be interpreted as follows: If it is
+          ## ".", use all channels except Time.  Otherwise, use
+          ## f[,channels], where f is a flow frame
+
+
+          column.names <- colnames(data)
+          if (channels == as.symbol("."))
+          {
+              if (exclude.time)
+                  column.names <- column.names[column.names != time]
+          }
+          else
+              column.names <- eval(channels) ## e.g. c("FSC-H", "Time")
+
+          prepanel.parallel.flowset <- 
+              function(x, frames, column.names, ...)
+              {
+                  x <- as.character(x)
+                  if (length(x) > 1) stop("must have only one flow frame per panel")
+                  if (length(x) < 1) return()
+                  nm <- x
+                  z <- as.data.frame(exprs(frames[[nm]])[, column.names])
+                  prepanel.default.parallel(z = z)
+              }
+
+          panel.parallel.flowset <-
+              function(x, 
+                       frames, channel, column.names, 
+                       ...)
+              {
+                  x <- as.character(x)
+                  if (length(x) > 1) stop("must have only one flow frame per panel")
+                  if (length(x) < 1) return()
+
+                  nm <- x
+                  z <- as.data.frame(exprs(frames[[nm]])[, column.names])
+                  if (!is.null(filterResults))
+                  {
+                      this.filter.result <- filterResults[[nm]]
+                      groups <- this.filter.result@subSet
+                  }
+                  else groups <- NULL
+                  panel.parallel(1, 1, z = z,
+                                 groups = groups,
+                                 subscripts = seq_len(nrow(z)),
+                                 ...)
+              }
+
+          densityplot(x, data = pd,
+                      prepanel = prepanel.parallel.flowset,
+                      panel = panel.parallel.flowset,
+                      frames = data@frames,
+                      column.names = column.names,
+                      xlab = xlab, ylab = ylab,
+                      default.scales =
+                      list(x = list(at = c(0, 1), labels = c("Min", "Max")),
+                           y =
+                           list(alternating = FALSE, axs = "i", tck = 0,
+                                at = seq_along(column.names),
+                                labels = column.names)),
+                      ...)
+      })
+
