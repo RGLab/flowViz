fbe13b83d61fbd041668e7a934a63d212386372e
Deepayan Sarkar <dsarkar@fhcrc.org>
2007-08-29 21:33:20 +0000

    separated out panel function for xyplot.flowset, and removed rgl as a dependency
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowViz@26750 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xyplot.R
+++ R/xyplot.R
@@ -1,5 +1,97 @@
 
 
+prepanel.xyplot.flowset <- 
+    function(x, 
+             frames, channel.x, channel.y,
+             ...)
+{
+    if (length(nm <- as.character(x)) > 1)
+        stop("must have only one flow frame per panel")
+    if (length(nm) == 1)
+    {
+        xx <- evalInFlowFrame(channel.x, frames[[nm]])
+        yy <- evalInFlowFrame(channel.y, frames[[nm]])
+        list(xlim = range(xx, finite = TRUE),
+             ylim = range(yy, finite = TRUE),
+             dx = diff(xx), dy = diff(yy))
+    }
+    else list()
+}
+
+panel.xyplot.flowset <-
+    function(x, 
+             frames, channel.x, channel.y,
+             filter = NULL,
+             filterResults = NULL,
+
+             pch, smooth,
+             ...)
+{
+    x <- as.character(x)
+    if (length(x) > 1) stop("must have only one flow frame per panel")
+    if (length(x) < 1) return()
+
+    nm <- x
+    xx <- evalInFlowFrame(channel.x, frames[[nm]])
+    yy <- evalInFlowFrame(channel.y, frames[[nm]])
+
+    ## this.filter.result represents result of applying
+    ## filter, which is not necessarily
+    ## filterResults[[nm]] (and we have no way of
+    ## knowing if it is, so we will always need to
+    ## recompute it).  However, it is only required if
+    ## filter is specified, and then only for certain
+    ## types of filters (specifically, those that have a
+    ## 2D geometric representation), and even then only
+    ## if the parameters match.  All these decisions are
+    ## made inside the call to filterBoundary() later,
+    ## but we define the 'this.filter.result' variable
+    ## now because we don't want to compute it twice.
+
+    this.filter.result <- NULL
+
+    groups <- 
+        if (!is.null(filterResults))
+        {
+            filterResults[[nm]]@subSet
+        }
+        else if (!is.null(filter))
+        {
+            this.filter.result <- filter(frames[[nm]], filter)
+            this.filter.result@subSet
+        }
+        else NULL
+
+    if (smooth) {
+        panel.smoothScatter(xx, yy, ...)
+    }
+    else panel.xyplot(xx, yy, pch = pch,
+                      groups = groups,
+                      subscripts = seq_along(groups),
+                      ...)
+
+    if (!is.null(filter) && (is.list(displayFilter) || displayFilter))
+    {
+        display.pars <- if (is.list(displayFilter)) displayFilter else list(border = TRUE)
+        filter.boundary <-
+            filterBoundary(filter = filter,
+                           parameters = c(channel.x.name, channel.y.name),
+                           frame = frames[[nm]],
+                           result = this.filter.result)
+        do.call(panel.polygon,
+                c(filter.boundary, display.pars))
+    }
+    
+    ##                   if(displayFilter) {
+    ##                       if(class(filter)!="rectangleGate") {stop("Only rectangleGate is supported for displayFilter")}			       
+    ##                       hLine=c(attr(filter,"min")[gsub("`","",paste(channel.y))],attr(filter,"max")[gsub("`","",paste(channel.y))])
+    ##                       vLine=c(attr(filter,"min")[gsub("`","",paste(channel.x))],attr(filter,"max")[gsub("`","",paste(channel.x))])
+    ##                       if(!sum(is.na(hLine))) {panel.abline(h=hLine,col="red")}
+    ##                       if(!sum(is.na(vLine))) {panel.abline(v=vLine,col="red")}
+    ##                   }
+    
+}
+
 
 
 setMethod("xyplot",
@@ -35,98 +127,6 @@ setMethod("xyplot",
           channel.y.name <- expr2char(channel.y)
           channel.x <- as.expression(channel.x)
           channel.y <- as.expression(channel.y)
-
-          prepanel.xyplot.flowset <- 
-              function(x, 
-                       frames, channel.x, channel.y,
-                       ...)
-              {
-                  if (length(nm <- as.character(x)) > 1)
-                      stop("must have only one flow frame per panel")
-                  if (length(nm) == 1)
-                  {
-                      xx <- evalInFlowFrame(channel.x, frames[[nm]])
-                      yy <- evalInFlowFrame(channel.y, frames[[nm]])
-                      list(xlim = range(xx, finite = TRUE),
-                           ylim = range(yy, finite = TRUE),
-                           dx = diff(xx), dy = diff(yy))
-                  }
-                  else list()
-              }
-
-          panel.xyplot.flowset <-
-              function(x, 
-                       frames, channel.x, channel.y,
-                       filter = NULL,
-                       filterResults = NULL,
-
-                       pch, smooth,
-                       ...)
-              {
-                  x <- as.character(x)
-                  if (length(x) > 1) stop("must have only one flow frame per panel")
-                  if (length(x) < 1) return()
-
-                  nm <- x
-                  xx <- evalInFlowFrame(channel.x, frames[[nm]])
-                  yy <- evalInFlowFrame(channel.y, frames[[nm]])
-
-                  ## this.filter.result represents result of applying
-                  ## filter, which is not necessarily
-                  ## filterResults[[nm]] (and we have no way of
-                  ## knowing if it is, so we will always need to
-                  ## recompute it).  However, it is only required if
-                  ## filter is specified, and then only for certain
-                  ## types of filters (specifically, those that have a
-                  ## 2D geometric representation), and even then only
-                  ## if the parameters match.  All these decisions are
-                  ## made inside the call to filterBoundary() later,
-                  ## but we define the 'this.filter.result' variable
-                  ## now because we don't want to compute it twice.
-
-                  this.filter.result <- NULL
-
-                  groups <- 
-                      if (!is.null(filterResults))
-                      {
-                          filterResults[[nm]]@subSet
-                      }
-                      else if (!is.null(filter))
-                      {
-                          this.filter.result <- filter(frames[[nm]], filter)
-                          this.filter.result@subSet
-                      }
-                      else NULL
-
-                  if (smooth) {
-                      panel.smoothScatter(xx, yy, ...)
-                  }
-                  else panel.xyplot(xx, yy, pch = pch,
-                                    groups = groups,
-                                    subscripts = seq_along(groups),
-                                    ...)
-
-                  if (!is.null(filter) && (is.list(displayFilter) || displayFilter))
-                  {
-                      display.pars <- if (is.list(displayFilter)) displayFilter else list(border = TRUE)
-                      filter.boundary <-
-                          filterBoundary(filter = filter,
-                                         parameters = c(channel.x.name, channel.y.name),
-                                         frame = frames[[nm]],
-                                         result = this.filter.result)
-                      do.call(panel.polygon,
-                              c(filter.boundary, display.pars))
-                  }
-                                    
-##                   if(displayFilter) {
-##                       if(class(filter)!="rectangleGate") {stop("Only rectangleGate is supported for displayFilter")}			       
-##                       hLine=c(attr(filter,"min")[gsub("`","",paste(channel.y))],attr(filter,"max")[gsub("`","",paste(channel.y))])
-##                       vLine=c(attr(filter,"min")[gsub("`","",paste(channel.x))],attr(filter,"max")[gsub("`","",paste(channel.x))])
-##                       if(!sum(is.na(hLine))) {panel.abline(h=hLine,col="red")}
-##                       if(!sum(is.na(vLine))) {panel.abline(v=vLine,col="red")}
-##                   }
-                  
-              }
 
           if (missing(xlab)) xlab <- channel.x.name
           if (missing(ylab)) ylab <- channel.y.name
