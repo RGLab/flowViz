11d8d4edad80c6fdc9e19392d1ca00e65ec16f3b
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2007-12-11 04:29:49 +0000

    time line plot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowViz@29141 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/timeLinePlot.R
@@ -0,0 +1,138 @@
+## Plot values of a flowSet against the time domain.
+## TO DO: Eventualy, we might want individual methods for flowSets and for
+## flowFrames, although it might be worthwhile to reimplement using
+## lattice graphics first...
+
+
+## run over a cytoFrame, bin the values according to the time domain
+## and compute medians for each bin. The resultof this function will
+## be the input to the plotting functions
+prepareSet <- function(x, parm){
+    cn <- colnames(x)
+    wcn <- grep("Time|time", cn)
+    if(length(wcn)==0)
+        stop("No time domain recording for this data")
+    else
+        ti <- wcn[1]
+    xx <- x[, ti]
+    ord <- order(xx)
+    xx <- xx[ord]
+    yy <- x[ord, parm]
+    lenx <- length(xx)
+    bs <- 500
+    if(lenx>bs){
+        bc <- floor(lenx/bs)
+        cf <- c(rep(1:bc, each=bs), rep(bc+1, lenx-bc*bs))
+        stopifnot(length(cf) == lenx)
+        tmpx <- split(xx,cf)
+        tmpy <- split(yy,cf)
+        yy <- sapply(tmpy, median, na.rm=TRUE)
+        xx <- sapply(tmpx, mean, na.rm=TRUE)
+    }else{
+        warning("Low number of events", call.=FALSE)
+        tmpy <- split(yy,xx)
+        yy <- sapply(tmpy, median, na.rm=TRUE)
+        xx <- unique(xx)
+    }
+    
+    return(cbind(xx,yy))
+}
+
+
+## wrapper function to produce the plots
+timeLinePlot <- function(x, par, type=c("stacked", "scaled", "native"), col,
+                         ylab=names(x), ...){
+    if(missing(col)){
+        require(RColorBrewer)
+        colp <- brewer.pal(12, "Paired")
+        col <- colorRampPalette(colp)(length(x))
+        set.seed(1000)
+        col <- sample(col)				 
+    }else{
+        if(length(col)!=1 || length(col)!=length(x))
+            stop("'col' must be color vector of length 1 or same length ",
+                 "as the flowSet")
+    }
+    timeData <-  fsApply(x, prepareSet, par, use.exprs=TRUE, simplify=FALSE)
+    type <- match.arg(type)
+    mr <- range(x[[1]])[par,]
+    mr[1] <- max(mr[1], 0)
+    med <- sapply(timeData, function(z) median(z[,2], na.rm=TRUE))
+    switch(type, 
+        "scaled"=scaledPlot(timeData, p=par, range=mr, col=col, med=med, ...),
+        "stacked"=stackedPlot(timeData, p=par, range=mr, col=col, ylab=ylab,
+        med=med, ...),
+        "native"=nativePlot(timeData, p=par, range=mr, col=col, med=med, ...),
+        stop("Unknown type"))
+    ad <- mapply(function(z,m) sum(abs(z[,2]-m), na.rm=TRUE)/nrow(z),
+                 timeData, med)
+    opar <- par(mar=c(5,3,0,3), las=2)
+    barplot(ad, axes=FALSE, col=col, cex.names=0.8, ylim=c(0, mr[2]/25))
+    box(col="darkgray")
+    par(opar)
+    return(invisible(NULL))
+}
+
+
+## align values around 0 and plot
+scaledPlot <- function(y, p, main=paste("time line for", p),
+                       range, col, med, ...){
+    layout(matrix(1:2), heights=c(0.8, 0.2))
+    par(mar=c(3.5,2.5,3,2.5), mgp=c(1.5,0.5,0))
+    y <- mapply(function(z, m) data.frame(x=z[,1], y=z[,2]-m), y, med,
+                SIMPLIFY=FALSE)
+    maxX <- max(sapply(y, function(z) max(z[,1], na.rm=TRUE)), na.rm=TRUE)
+    xlim <- c(0, maxX)
+    maxY <- max(sapply(y, function(z) max(abs(range(z[,2], na.rm=TRUE)), 
+                                          na.rm=TRUE)), na.rm=TRUE)
+    minRange <- diff(range)/4
+    ylim <- c(min(-maxY, -minRange), max(maxY, minRange))
+    plot(y[[1]], xlab="time", type="l", col=col[1], 
+         lwd=1, xlim=xlim, ylim=ylim, main=main, ylab="", ...)
+    abline(h=0, col="darkgray")
+    for(j in 2:length(y))
+        lines(y[[j]], col=col[j], lwd=1)
+}
+
+
+## plot stacked values for each flowFrame
+stackedPlot <- function(y, p, main=paste("time line for", p),
+                        range, col, ylab, med, ...){
+    layout(matrix(1:2), heights=c(0.8, 0.2))
+    par(mar=c(3.5,3,3,3), mgp=c(1.5,0.5,0))
+    stacks <- 1:length(y) * (diff(range)/10)
+    y <- mapply(function(z, m, s) data.frame(x=z[,1], y=z[,2]-m+s), y, med, 
+			stacks, SIMPLIFY=FALSE)
+    maxX <- max(sapply(y, function(z) max(z[,1], na.rm=TRUE)), na.rm=TRUE)
+    xlim <- c(0, maxX)
+    maxY <- max(sapply(y, function(z) max(abs(range(z[,2], na.rm=TRUE)), 
+                                          na.rm=TRUE)), na.rm=TRUE)
+    ylim <- c(1, maxY)
+    if(missing(ylab))
+       ylab <- names(y)
+    plot(y[[1]], xlab="time", ylab="", type="l", col=col[1], 
+         lwd=1, xlim=xlim, ylim=ylim, main=main, yaxt="n", ...)
+    if(length(ylab)>1)
+        axis(2, stacks, ylab, cex.axis=0.8)
+    for(j in 2:length(y))
+        lines(y[[j]], col=col[j], lwd=1)
+}
+
+
+## plot values in the "native" dimensions
+nativePlot <- function(y, p, main=paste("time line for", p),
+                       range, col, med, ...){
+    layout(matrix(1:2), heights=c(0.8, 0.2))
+    par(mar=c(3.5,2.5,3,2.5), mgp=c(1.5,0.5,0))
+    maxX <- max(sapply(y, function(z) max(z[,1], na.rm=TRUE)), na.rm=TRUE)
+    xlim <- c(0, maxX)
+    maxY <-  max(sapply(y, function(z) max(abs(range(z[,2], na.rm=TRUE)), 
+                                           na.rm=TRUE)), na.rm=TRUE)
+    minRange <- diff(range)/4
+    ylim <- c(0, max(minRange, maxY))
+    plot(y[[1]], xlab="time", ylab="", type="l", col=col[1], 
+         lwd=1, xlim=xlim, ylim=ylim, main=main, ...)
+    for(j in 2:length(y))
+        lines(y[[j]], col=col[j], lwd=1)
+}
+
