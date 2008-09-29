4b177d1d3339d5092237113a78b37be1ddcbdc31
Robert Gentleman <rgentlem@fhcrc.org>
2008-09-29 23:51:16 +0000

    updating the R code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowViz@34059 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hexbin.R
+++ R/hexbin.R
@@ -7,11 +7,13 @@ hexbin <- function(x, y, bins = 50) {
     if( any(x > 1 ) | any(x < 0 ) ) stop("x values must be in 0,1")
     if( any(y > 1 ) | any(y < 0 ) ) stop("y values must be in 0,1")
     nB = (bins+20)^2
+    xbin = rep(0, nB)
+    ybin = rep(0, nB)
+    counts = rep(0L, nB)
     res = .Call("binHex", as.integer(length(x)),  x = as.double(x), 
        y = as.double(y), as.integer(bins), as.integer(nB), 
-       counts = vector("integer", length = nB), 
-       xbin = vector("double", length=nB),
-       ybin = vector("double", length=nB), PACKAGE="flowViz")
+       counts = as.integer(counts),
+       xbin = xbin, ybin = ybin, PACKAGE="flowViz")
 
    return(list(counts=res$counts, xbin = res$xbin, ybin=res$ybin))
 }
